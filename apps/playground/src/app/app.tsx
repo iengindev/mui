df59e3895a7e2756390130cbfc4f93ddd4ba3bca
Ilyas <weendibol21300@gmail.com>
2025-11-27 12:00:59 +0500

    Update package-lock.json with new integrity hash for @mui/core and add size variants to MButton component in the playground app for enhanced customization.

--- apps/playground/src/app/app.tsx
+++ apps/playground/src/app/app.tsx
@@ -5,7 +5,9 @@ import { MButton } from '@mui/core'
 export default defineComponent(() => {
   return () => (
     <>
+      <MButton label='Primary' size='small'/>
       <MButton label='Primary'/>
+      <MButton label='Primary' size='large'/>
     </>
   )
 })
