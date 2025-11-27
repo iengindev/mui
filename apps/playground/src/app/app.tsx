66738abe6aef21f7e84804ba027f17e49bde5e58
Ilyas <weendibol21300@gmail.com>
2025-11-27 15:19:56 +0500

    Update MButton component in playground app to include multiple severity options and update package-lock.json with new integrity hash for @mui/core.

--- apps/playground/src/app/app.tsx
+++ apps/playground/src/app/app.tsx
@@ -5,9 +5,33 @@ import { MButton } from '@mui/core'
 export default defineComponent(() => {
   return () => (
     <>
-      <MButton label='Primary' size='small'/>
-      <MButton label='Primary'/>
-      <MButton label='Primary' size='large'/>
+      <MButton label='Submit' size='small'/>
+      <MButton label='Submit'/>
+      <MButton label='Submit' size='large'/>
+
+      <MButton label='Submit' severity='secondary' size='small'/>
+      <MButton label='Submit' severity='secondary'/>
+      <MButton label='Submit' severity='secondary' size='large'/>
+
+      <MButton label='Submit' severity='info' size='small'/>
+      <MButton label='Submit' severity='info'/>
+      <MButton label='Submit' severity='info' size='large'/>
+
+      <MButton label='Submit' severity='success' size='small'/>
+      <MButton label='Submit' severity='success'/>
+      <MButton label='Submit' severity='success' size='large'/>
+
+      <MButton label='Submit' severity='warning' size='small'/>
+      <MButton label='Submit' severity='warning'/>
+      <MButton label='Submit' severity='warning' size='large'/>
+
+      <MButton label='Submit' severity='danger' size='small'/>
+      <MButton label='Submit' severity='danger'/>
+      <MButton label='Submit' severity='danger' size='large'/>
+
+      <MButton label='Submit' severity='help' size='small'/>
+      <MButton label='Submit' severity='help'/>
+      <MButton label='Submit' severity='help' size='large'/>
     </>
   )
 })
