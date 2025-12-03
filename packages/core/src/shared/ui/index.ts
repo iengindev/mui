af17143a7959614b67f4475b7e578ff680ba653d
Ilyas <weendibol21300@gmail.com>
2025-12-03 18:36:12 +0500

    Enhance index export in UI library by adding type exports for MIcon and MButton components, improving type management for icon and button props.

--- packages/core/src/shared/ui/index.ts
+++ packages/core/src/shared/ui/index.ts
@@ -1,4 +1,7 @@
 import '@/assets/styles/main.style.css'
 
 export { default as MIcon } from './icon/icon.component'
+export type { IconPorpsType as MIconPorpsType } from './icon/icon.types'
+
 export { default as MButton } from './button/button.component'
+export type { ButtonPorpsType as MButtonPorpsType, ButtonEmitsType as MButtonEmitsType } from './button/button.types'
