c0815c6dcdd37c684e148a1b892b3737566a52ee
Ilyas <weendibol21300@gmail.com>
2025-12-07 18:57:37 +0500

    Refactor button group component and service to use a class-based structure, improving method organization and encapsulation for better component rendering.

--- packages/core/src/shared/ui/button-group/button-group.component.tsx
+++ packages/core/src/shared/ui/button-group/button-group.component.tsx
@@ -2,16 +2,16 @@ import './button-group.style.css'
 
 import { defineComponent, type SetupContext } from 'vue'
 
-import { useButtonGroupService } from './button-group.service'
+import { ButtonGroupService } from './button-group.service'
 
 export default defineComponent((_, ctx: SetupContext) => {
-  const { methods } = useButtonGroupService(_, ctx)
+  const useButtonGroupService = new ButtonGroupService(ctx)
 
   return () => (
     <>
       <div class="mui-btn-group">
         {
-          methods.components()
+          useButtonGroupService.methods().components()
         }
       </div>
     </>
--- packages/core/src/shared/ui/button-group/button-group.service.tsx
+++ packages/core/src/shared/ui/button-group/button-group.service.tsx
@@ -1,9 +1,15 @@
 import { renderSlot, type SetupContext } from 'vue'
 
-export const useButtonGroupService = (_: Record<string, any>, ctx: SetupContext) => ({
-  methods: {
-    components: () => {
-      return renderSlot(ctx.slots, 'default')
-    },
-  },
-})
+export class ButtonGroupService {
+  constructor(
+    public ctx: SetupContext
+  ) {}
+
+  methods() {
+    return {
+      components: () => {
+        return renderSlot(this.ctx.slots, 'default')
+      }
+    }
+  }
+}
