9906c3694d546c4cd0b2ebfd942c25c7ad5329df
Ilyas <weendibol21300@gmail.com>
2025-12-07 17:48:48 +0500

    Add MButtonSelect component and types to index export for enhanced button selection functionality

--- packages/core/src/shared/ui/index.ts
+++ packages/core/src/shared/ui/index.ts
@@ -7,3 +7,6 @@ export { default as MButton } from './button/button.component'
 export type { ButtonPorpsType as MButtonPorpsType, ButtonEmitsType as MButtonEmitsType } from './button/button.types'
 
 export { default as MButtonGroup } from './button-group/button-group.component'
+
+export { default as MButtonSelect } from './button-select/button-select.component'
+export type { ButtonSelectPorpsType as MButtonSelectPorpsType, ButtonSelectEmitsType as MButtonSelectEmitsType } from './button-select/button-select.types'
