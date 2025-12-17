edd1b6156b4f7d36ca22be48d7fe719b92798c49
Ilyas <weendibol21300@gmail.com>
2025-12-17 10:07:16 +0500

    Add MIcon component and its type definition to shared UI exports
    - Exported MIcon component from the icon directory for easier access.
    - Added MIconPropsType to define the properties for the MIcon component, enhancing type safety and usability.

--- apps/playground/src/shared/ui/index.ts
+++ apps/playground/src/shared/ui/index.ts
@@ -1,2 +1,5 @@
+export { default as MIcon } from './icon/icon.component'
+export type { IconPropsType as MIconPropsType } from './icon/icon.types'
+
 export { default as MButton } from './button/button.component'
 export type { ButtonPropsType as MButtonPropsType, ButtonEmitsType as MButtonEmitsType } from './button/button.types'
