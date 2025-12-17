124a519da69df6fe9aca58f5d92e63a008c46dd3
Ilyas <weendibol21300@gmail.com>
2025-12-17 10:35:02 +0500

    Add MButtonGroup usage in app component
    - Integrated MButtonGroup into the app component, including multiple MButton instances for enhanced button group functionality and user interaction.

--- apps/playground/src/app/app.tsx
+++ apps/playground/src/app/app.tsx
@@ -2,11 +2,18 @@ import { defineComponent } from 'vue'
 
 import { RouterView } from 'vue-router'
 
+import { MButtonGroup } from '@/shared/ui'
 import { MButton } from '@/shared/ui'
 
 export default defineComponent(() => {
   return () => (
     <>
+      <MButtonGroup>
+        <MButton label='test 1'/>
+        <MButton label='test 2'/>
+        <MButton label='test 3'/>
+        <MButton label='test 4'/>
+      </MButtonGroup>
     </>
   )
 })
